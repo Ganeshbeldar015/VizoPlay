87e888e339efd7a0764a7d9fe87d7a301c9beb63
ganeshbeldar015 <ganeshbeldar015@gmail.com>
2025-09-11 21:22:11 +0530

    Initial commit

---
+++ learn/src/components/NetflixSeries.jsx
@@ -0,0 +1,14 @@
+import SeriesData from "../api/SeriesData.json";
+import {SeriesCard} from "./SeriesCard";
+
+export const NetflixSeries = () => {
+  return (
+    <ul className="grid grid-three--cols">  
+      {SeriesData.map((curElem) => (
+        <SeriesCard key={curElem.id} data={curElem} />
+      ))}
+    </ul>
+  );
+};
+
+export default NetflixSeries; 
---
+++ learn/src/components/SeriesCard.jsx
@@ -0,0 +1,32 @@
+import styles from "./Netflix.module.css";
+
+export const SeriesCard = ({ data }) => {
+  const { img_url, name, rating, description, genre, cast, watch_url } = data;
+
+  const btn_style = {
+    padding: "1.2rem 2.4rem",
+    border: "none",
+    fontSize: "1.6rem",
+    backgroundColor: `${rating >= 8.5 ? "#7dcea0": "#f7dc6f"}`,
+    color: "var(--bg-color)",
+    cursor: "pointer",
+  };
+const ratingClass= rating >= 8.5 ? styles.super_hit : styles.average;
+  return (
+    <li className="{styles.card}">
+      <div>
+        <img src={img_url} alt="qot.jpg" width="40%" height="40%" />
+      </div>
+      <div className={styles["card-content"]}>
+        <h2>Name: {name}</h2>
+        <h3>Rating: <span className={`${styles.rating} ${ratingClass}`}>{rating}</span> </h3>
+        <p>Summary: {description} </p>
+        <p>Genre: {genre} </p>
+        <p>Cast: {cast} </p>
+        <a href={watch_url} target="_blank">
+          <button style={btn_style}>Watch Now</button>
+        </a>
+      </div>
+    </li>
+  );
+};
