170a3098ef5400ad75f54ca236ae25b9fdd0a5f7
ganeshbeldar015 <ganeshbeldar015@gmail.com>
2025-09-28 02:40:58 +0530

    update

--- learn/src/components/SeriesCard.jsx
+++ learn/src/components/SeriesCard.jsx
@@ -17,10 +17,11 @@ const ratingClass= rating >= 8.5 ? styles.super_hit : styles.average;
       <div>
         <img src={img_url} alt="qot.jpg" width="40%" height="40%" />
       </div>
-      <div className={styles["card-content"]}>
+      {/* <div className={styles["card-content"]}> */}
+      <div className="flex flex-col gap-6 py-[3.2rem] px-[1.2rem]">
         <h2>Name: {name}</h2>
         <h3>Rating: <span className={`${styles.rating} ${ratingClass}`}>{rating}</span> </h3>
-        <p>Summary: {description} </p>
+        <p className="text-3xl font-bold underline text-cyan-300">Summary: {description} </p>
         <p>Genre: {genre} </p>
         <p>Cast: {cast} </p>
         <a href={watch_url} target="_blank">
